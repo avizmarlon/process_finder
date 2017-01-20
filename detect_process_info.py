52724c3888935def19cd9bf99079886faf1dfd9d
Marlon Aviz <aviz.marlon@gmail.com>
2017-01-19 23:48:57 -0200

    fixed multiple process insertion in dict and other bugs

--- detect_process_info.py
+++ detect_process_info.py
@@ -1,23 +1,38 @@
 import re
 import os
+import pyperclip
+import time
 
 # Dict Version
 pids = {}
 proc_list = os.popen('tasklist').readlines()
 
 for proc in proc_list:
-    # finds all pids of the current iteration
-    pid = re.findall('.* ([0-9]+) [a-zA-Z]', proc)
-
     # finds all the process names of the current iteration
-    pname = re.findall('^\S+', proc)
-
-    # this section is in the case that a process has multiple instances and thus multiple pids with the same pname
-    if pname in pids == True:
+    pname = re.findall('^(.*?) [0-9]+', proc)
+    if len(pname) == 0:
+        continue
+    pname = pname[0].strip()
+    
+    # finds all pids of the current iteration
+    pid = re.findall('.* ([0-9]+) [a-zA-Z]+', proc)
+    
+    # this section is in the case that a process has multiple instances and thus multiple pids with the same pname    
+    if pname in pids:
         if type(pids[pname]) == list:
             pids[pname].extend(pid)
+            
         # pname value becomes a list in case its not a list yet
-        current_pname_value = pids[pname]
-        pids[pname] = [current_pname_value, pid]
+        else:
+            current_pname_value = pids[pname]
+            pids[pname] = [current_pname_value, pid]
     else:
         pids[pname] = pid
+ 
+ 
+# for tests
+for process in pids:
+    print("Process:", process, "\nPID(s):")
+    for pid in pids[process]:
+        print(pid)
+    print('\n')
